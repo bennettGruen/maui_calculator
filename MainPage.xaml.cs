ca3e69581efe9b364ff63b3aa6e461b4857dee1e
Bennett Grün <metheny2admire@hotmail.de>
2024-04-29 00:12:02 +0200

    Projektdateien hinzufügen.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace maui_calculator
+{
+    public partial class MainPage : ContentPage
+    {
+       // int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            labelResult.Text += "X";
+
+            //SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
